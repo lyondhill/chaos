b83b3ada9c56a0db62868469e3590011f3e8bf49
Lyon <lyondhill@gmail.com>
2017-02-02 15:47:02 -0700

    update

--- chaos.go
+++ chaos.go
@@ -1,39 +1,39 @@
 package chaos
 
 import (
-
+	"fmt"
+	"math/rand"
 )
 
-type (
-	Procedure interface{
-		Name() string
-		Runnable() bool
-		Run() error
-		Success() bool
-	}
-)
+type Procedure interface {
+	Name() string
+	Runnable() bool
+	Run() error
+	Success() bool
+}
 
 var procedures = []Procedure{}
 
 // Register a new procedure you would like to run
-func Register(proc Procedure) {
-	procedures = append(Procedure, proc)
+func Register(pr Procedure) {
+	procedures = append(procedures, pr)
 }
 
 // start running procedures in a random order and run until failure
 func Start() error {
 	for {
-		for _, proc := range procedures {
-			if proc.Runnable() {
-				err := proc.Run()
-				if err != nil {
-					return err
-				}
-				if !proc.Success() {
-					return fmt.Errorf("process (%s) ran but is not successful", proc.Name())
-				}
+		proc := procedures[rand.Intn(len(procedures))]
+		if proc.Runnable() {
+			fmt.Println("\n\n\nNow running:%s\n\n", proc.Name())
+
+			err := proc.Run()
+			if err != nil {
+				return err
+			}
+
+			if !proc.Success() {
+				return fmt.Errorf("process (%s) ran but is not successful", proc.Name())
 			}
 		}
 	}
 }
-
